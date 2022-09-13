ab1bdbd5698800b5a536574c274d29fe09034c2a
electrictangent <electrictangent@protonmail.com>
2022-09-13 19:23:14 +1200

    Added dog subclass

--- animal-classes.py
+++ animal-classes.py
@@ -7,6 +7,11 @@ class Animal():
         else:
             return False
 
+class Dogs(Animal):
+    def __init__(self, colour, age):
+        self.colour = colour
+        Animal.__init__(self, age)
+
 def test_animal_class():
     fox = Animal(12)
     print("Object fox should return 12 for age\n  Result: {}".format(fox.age))
@@ -14,6 +19,10 @@ def test_animal_class():
     print("Object fox should return 1 for age\n  Result: {}".format(fox.age))
     print("Object fox should return False for age\n  Result: {}".format(fox.isBaby()))
 
+def test_dog_subclass():
+    dog = Dogs("Red", 12)
+    print("Object dog should return 12 for age\n  Result: {}".format(fox.age))
 
 if __name__ == "__main__":
     test_animal_class()
+    test_dog_subclass()
