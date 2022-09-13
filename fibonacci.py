d9605800f5e86d56855379e571dd780f149cca8e
electrictangent <electrictangent@protonmail.com>
2022-09-13 18:59:34 +1200

    Refactored code Changed name for function fib() to fibonacci() Added extra formatting to test function to make test results more readable

--- fibonacci.py
+++ fibonacci.py
@@ -1,15 +1,15 @@
 def test_fib_func():
-    print(fib(0))
-    print(fib(1))
-    print(fib(3))
+    print("Result should be 0\n  Result: {}".format(fibonacci(0)))
+    print("Result should be 1\n  Result: {}".format(fibonacci(1)))
+    print("Result should be 2\n  Result: {}".format(fibonacci(3)))
 
-def fib(n):
+def fibonacci(n):
     if n == 0:
         return 0
     if n == 1:
         return 1
     else:
-        return fib(n-1) + fib(n-2)
+        return fibonacci(n-1) + fibonacci(n-2)
 
 if __name__ == "__main__":
     test_fib_func()
