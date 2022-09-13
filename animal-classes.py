ae9c78138e7e56a37562b312c51008c64dd81a04
electrictangent <electrictangent@protonmail.com>
2022-09-13 19:21:50 +1200

    Added function for checking if animal is baby

--- animal-classes.py
+++ animal-classes.py
@@ -1,13 +1,19 @@
 class Animal():
     def __init__(self, age):
         self.age = age
-
+    def isBaby(self):
+        if self.age < 1:
+            return True
+        else:
+            return False
 
 def test_animal_class():
     fox = Animal(12)
     print("Object fox should return 12 for age\n  Result: {}".format(fox.age))
     fox.age = 1
     print("Object fox should return 1 for age\n  Result: {}".format(fox.age))
+    print("Object fox should return False for age\n  Result: {}".format(fox.isBaby()))
+
 
 if __name__ == "__main__":
     test_animal_class()
